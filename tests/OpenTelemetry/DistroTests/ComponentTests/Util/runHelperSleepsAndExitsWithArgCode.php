57026adc09fa4c39133cfd04c2978d88741413fa
Pawel Filipczak <pawel.filipczak@elastic.co>
2026-02-25 00:19:43 +0100

    Fixed path

--- tests/OpenTelemetry/DistroTests/ComponentTests/Util/runHelperSleepsAndExitsWithArgCode.php
+++ tests/OpenTelemetry/DistroTests/ComponentTests/Util/runHelperSleepsAndExitsWithArgCode.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 namespace OpenTelemetry\DistroTests\ComponentTests\Util;
 
-require __DIR__ . '/../../../bootstrap.php';
+require __DIR__ . '/../../../../bootstrap.php';
 
 HelperSleepsAndExitsWithArgCode::run();
